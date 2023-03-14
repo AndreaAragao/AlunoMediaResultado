c88cedc4ca93fcc9e451fb778dfb84f925dd430c
Andréa <andreamaragao@gmail.com>
2023-03-13 22:42:09 -0300

    Adicionar arquivos de projeto.

---
+++ App/Aluno.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projetos07.App
+{
+    internal class Aluno
+    {
+        public string Nome { get; private set; }
+        public double Nota1 { get; private set; }
+        public double Nota2 { get; private set; }
+        public double Nota3 { get; private set; }
+
+        public Aluno(string nome, double nota1, double nota2, double nota3)
+        {
+            Nome = nome;
+            Nota1 = nota1;
+            Nota2 = nota2;
+            Nota3 = nota3;
+        }
+
+        private double calcularMedia()
+        {
+            return (Nota1 + Nota2 + Nota3) / 3;
+        }
+
+        private string calcularResultado()
+        {
+            if (calcularMedia() < 4)
+            {
+                return "Reprovado";
+            }
+            else if (calcularMedia() >= 4 && calcularMedia() < 6)
+            {
+                return "Exame Especial";
+            }
+            else
+            {
+                return "Aprovado";
+            }
+        }
+
+        public override string ToString()
+        {
+            //return nome + " média " + (calcularMedia()).tostring("f2") + " " + calcularResultado(); 
+            return $"{Nome} média {calcularMedia().ToString("F2")} {calcularResultado()}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using Projetos07.App;
+using System;
+
+namespace Projetos07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcao;
+            do
+            {
+                Console.WriteLine("Digite 1 para avaliar um aluno ou 0 para sair: \n");
+                opcao = int.Parse(Console.ReadLine());
+                if(opcao == 1)
+                {
+                    string nome;
+                    double nota1, nota2, nota3;
+
+                    Console.WriteLine("\nDigite o nome do aluno:");
+                    nome = Console.ReadLine();
+
+                    Console.WriteLine("\nDigite a primeira nota:");
+                    nota1 = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("\nDigite a segunda nota:");
+                    nota2 = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("\nDigite a terceira nota:");
+                    nota3 = double.Parse(Console.ReadLine());
+
+                    Aluno aluno = new Aluno(nome, nota1, nota2, nota3);
+
+                    Console.WriteLine(aluno);
+                }
+                else
+                {
+                    opcao = 0;
+                }
+            }
+            while (opcao == 1);
+        }
+    }
+}
